02971174c656e06b4d79732dbc581ad8dbf67403
Shubo Zhang <bryanshubo@gmail.com>
2015-06-17 08:06:06 -0400

    test consumerAdapter

--- Consumer/src/main/java/JMS/listener/ConsumerL.java
+++ Consumer/src/main/java/JMS/listener/ConsumerL.java
@@ -4,6 +4,7 @@ import JMS.adapter.ConsumerAdapter;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.jms.core.JmsTemplate;
 
